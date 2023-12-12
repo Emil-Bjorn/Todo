11d830fa819800f0b8f2bc05b145466ee46895bb
Emil <emilbjorn101@gmail.com>
2023-12-12 18:29:56 +0100

    Added save functionality

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
   $(document).ready(function () {
     //itemIndex used to connect label and checkbox
     let itemIndex = 0;
+    if (localStorage.getItem("itemIndex") != null) {
+      itemIndex = localStorage.getItem("itemIndex");
+    };
+
+    //taskArray used to save and retrieve tasks from local storage
+    let taskArray = [];
+    if (localStorage.getItem("taskArray") != null) {
+      taskArray = JSON.parse(localStorage.getItem("taskArray"));
+    }
+
+    //Loops through taskArray and adds saved tasks to list
+    function initializeList() {
+      taskArray.forEach((task) => {
+        //Creates all the elements needed for a new row
+        let newListRow = document.createElement("div");
+        let newListItem = document.createElement("label");
+        let newCheckBox = document.createElement("input");
+        let newTrash = document.createElement("button");
+        let newLineBreak = document.createElement("hr")
+        newListRow.classList.add("listRow");
+
+        //Links the label and checkbox together making the label clickable
+        newCheckBox.setAttribute("id", task.id);
+        newListItem.setAttribute("for", task.id);
+        newCheckBox.setAttribute("type", "checkbox");
+        newListItem.textContent = task.label;
+        newTrash.classList.add("trashCan");
+        newLineBreak.classList.add("lineBreak");
+        $(newListRow).append(newCheckBox);
+        $(newListRow).append(newListItem);
+        $(newListRow).append(newTrash);
+        $(newListRow).append(newLineBreak);
+        $("#list").append(newListRow);
+      })
+    }
+
     //Listens for ENTER to be pressed in input field, if ENTER is pressed the addTask function is run
     $("#new").keydown(function (e) {
         if (e.which == 13){
@@ -25,10 +61,12 @@
         let newTrash = document.createElement("button");
         let newLineBreak = document.createElement("hr")
         newListRow.classList.add("listRow");
+
         //Links the label and checkbox together making the label clickable
         newCheckBox.setAttribute("id", itemIndex);
         newListItem.setAttribute("for", itemIndex);
         itemIndex++;
+        localStorage.setItem("itemIndex",itemIndex);
         newCheckBox.setAttribute("type", "checkbox");
         newListItem.textContent = $("#new").val();
         newTrash.classList.add("trashCan");
@@ -38,6 +76,11 @@
         $(newListRow).append(newTrash);
         $(newListRow).append(newLineBreak);
         $("#list").append(newListRow);
+
+        //Saves new task to local storage
+        let taskObj = {"id":newCheckBox.getAttribute("id"), "label":newListItem.textContent};
+        taskArray.push(taskObj);
+        localStorage.setItem("taskArray", JSON.stringify(taskArray));
 
         //Clears input field after inputting a new list item
         document.getElementById("new").value = "";
@@ -51,8 +94,13 @@
       return str.trim().length;
     }
 
-    //Deletes row when trash can is clicked
+    //Deletes row when trash can is clicked and removes task from taskArray
     $(document).on("click", ".trashCan", function() {
+      let deletedTaskID = this.parentNode.firstChild.id
+      taskArray = taskArray.filter(function(task) {
+        return task.id != deletedTaskID;
+      })
+      localStorage.setItem("taskArray", JSON.stringify(taskArray));
       this.parentNode.remove();
     });
 
@@ -70,6 +118,8 @@
     //Calls the getDate function when the page loads and then continues to call the function every second so that the date will update when it changes
     getDate();
     setInterval(getDate, 1000);
+
+    initializeList();
 
   });
 
